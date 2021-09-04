7aafdc64e0b7f9cc996384165adb2f8413f37cb5
Dominique Pendjol <d.pendjol@gmail.com>
2021-09-04 22:48:38 +0200

    Finished Challenge Methods

---
+++ challengeMethods/challengeMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace challengeMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create 3 variables which contain names
+            string nameOne = "Frank";
+            string nameTwo = "Henk";
+            string nameThree = "Piet";
+            
+            // Call the method GreetFriend
+            GreetFriend(nameOne);
+            GreetFriend(nameTwo);
+            GreetFriend(nameThree);
+        }
+
+        /// <summary>
+        /// Writes a greeting to the console
+        /// </summary>
+        /// <param name="name">firstname of a person.</param>
+        static void GreetFriend(string name)
+        {
+            Console.WriteLine($"Hey {name}, how are you doing?");
+        }
+    }
+}
