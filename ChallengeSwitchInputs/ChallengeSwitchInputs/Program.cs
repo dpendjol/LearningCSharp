06b7a9ab915a397daab91a9c5247948aba429df6
Dominique Pendjol <d.pendjol@gmail.com>
2021-09-08 17:41:54 +0200

    Added instructions to the Challenge SwitchInputs

--- ChallengeSwitchInputs/ChallengeSwitchInputs/Program.cs
+++ ChallengeSwitchInputs/ChallengeSwitchInputs/Program.cs
@@ -2,6 +2,30 @@
 
 namespace ChallengeSwitchInputs
 {
+    /*
+     * Create an application that takes 2 input values.
+     * 
+     * Any input value (1st input)
+     * 
+     * Asks the data type of the input value. (2nd input)
+     * 
+     * It will print to the console the options like below to take input for the 2nd input value:
+     * 
+       * Press 1 for String
+       * 
+       * Press 2 for integer
+       * 
+       * Press 3 for Boolean
+       * 
+     * If the input value for the 2nd input is 1 then the application should check that if the entered 1st input is a valid string or not. Hereby we even want to check, if it is a complete alphabetic entry (so no numbers accepted)
+     * 
+     * If the input value for the 2nd input is 2 then the application should check if the entered 1st input is a valid integer or not.
+     * 
+     * Based on the input the 1st value and the selection of data type using the 2nd input, the application should return whether the entered 1st value is of data type selected by the user or not.
+     * 
+     * Please make sure to use a switch statement. To check the valid string you can write your custom logic.
+     */
+
     class Program
     {
         static void Main(string[] args)
@@ -60,7 +84,6 @@ namespace ChallengeSwitchInputs
                     break;
                 default:
                     Console.WriteLine("Please choose 1, 2 or 3.");
-                    
                     break;
 
             }
