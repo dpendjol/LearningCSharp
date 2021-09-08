1f16c3e9ec40b937ea2317af90fffb285a6bb937
Dominique Pendjol <d.pendjol@gmail.com>
2021-09-08 15:04:03 +0200

    Finished Challenge BoxProperties

---
+++ ChallengeBoxProperties/ChallengeBoxProperties/Box.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ChallengeBoxProperties
+{
+    class Box
+    {
+
+        // member variables
+        // Private member variables are only accessable from whitin the class itself.
+        // Public member variables are accessable from everywhere within the code
+        public int height;
+        //private int width;
+        private int length;
+
+        // properties
+        // We can use the default propertie creator
+        public int Width { get; set; }
+        // which is simular like
+        // in case of example below there must be a member variable called width
+        /*
+         * public int With 
+         * {
+         *      get
+         *      {
+         *          return width;
+         *      }
+         *      set
+         *      {
+         *          this.width = value;
+         *      }
+         */
+
+        // it is also possible to use properties to access member variables and preform checks on the set and modify the get
+        // for example below the value of this.length is always set positive.
+
+        public int Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    this.length = -value;
+                }
+                else
+                {
+                    this.length = value;
+                }
+            }
+        }
+
+        // we can also use properties to preform a calculation of modification when a get request is preformed.
+        public int Volume
+        {
+            get
+            {
+                return this.Length * this.Width * this.height;
+            }
+        }
+
+        public Box()
+        {
+
+        }
+    }
+}
---
+++ ChallengeBoxProperties/ChallengeBoxProperties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChallengeBoxProperties
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BoxProperties");
+            Box myBox = new Box();
+            myBox.Width = 2;
+            myBox.Length = 2;
+            myBox.height = 2;
+            Console.WriteLine("The volume of the box is: " + myBox.Volume);
+            myBox.Length = -4;
+            Console.WriteLine("The volume of the box is now: " + myBox.Volume);
+        }
+
+
+    }
+}
