43fd3ef7213c450a93bce7bbf65077f3f6ef1a1f
Dominique Pendjol <d.pendjol@gmail.com>
2021-09-26 20:46:14 +0200

    Started Challenge Inheritance Work, created base class Employee

---
+++ ChallengeInheritanceWork/ChallengeInheritanceWork/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeInheritanceWork
+{
+    /*
+     * Create a main class with the Main Method, then a base class Employee with the properties Name, FirstName, Salary, and the methods Work() and Pause().
+     */
+    class Employee
+    {
+        // properties
+        private string Name { get; set; }
+        private string FirstName { get; set; }
+        private double Salary { get; set; }
+
+        public Employee(string name, string firstname, double salary)
+        {
+            this.Name = name;
+            this.FirstName = firstname;
+            this.Salary = salary;
+        }
+        public void Work()
+        {
+            Console.WriteLine($"Employee {this.Name} is working.");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine($"Employee {this.Name} is having a break");
+        }
+
+    }
+}
---
+++ ChallengeInheritanceWork/ChallengeInheritanceWork/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+ * Create a main class with the Main Method, then a base class Employee with the properties Name, FirstName, Salary, and the methods Work() and Pause().
+ * Create a deriving class boss with the property CompanyCar and the method Lead().  Create another deriving class of employees - trainees with the properties WorkingHours and SchoolHours and a method Learn().
+ * Override the methods Work() of the trainee class so that it indicates the working hours of the trainee.
+ * Don’t forget to create constructors.
+ * Create an object of each of the three classes (with arbitrary values)
+ * and call the methods, Lead() of Boss and Work() of Trainee.
+ * 
+ * Just print out the respective text, what the respective employees do.
+ * E.g. Lead() should print out something like. I'm leading. It is up to you what you print out.
+ * 
+ */
+
+namespace ChallengeInheritanceWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Challenge Inheritance Work");
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine("Testing Employee class");
+            Employee employeeOne = new Employee("Disposer", "George", 200000.00);
+            employeeOne.Work();
+            employeeOne.Pause();
+        }
+    }
+}
