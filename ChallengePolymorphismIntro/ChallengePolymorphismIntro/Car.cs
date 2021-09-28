52dc39e0702c8fb7a8d793079c6e009d04b77329
Dominique Pendjol <d.pendjol@gmail.com>
2021-09-28 23:51:48 +0200

    Added instructions to Program file and created base class

---
+++ ChallengePolymorphismIntro/ChallengePolymorphismIntro/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengePolymorphismIntro
+{
+    class Car
+    {
+        // property that holds the horsepower
+        public int HP { get; set; }
+        public string Color { get; set; }
+        // property that holds the color
+        public Car(int hp, string color)
+        {
+            this.HP = hp;
+            this.Color = color;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"This car has {this.HP} horsepower and \n has a {this.Color} color.");
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine($"Car was repaired");
+        }
+    }
+}
--- ChallengePolymorphismIntro/ChallengePolymorphismIntro/Program.cs
+++ ChallengePolymorphismIntro/ChallengePolymorphismIntro/Program.cs
@@ -4,6 +4,14 @@ namespace ChallengePolymorphismIntro
 {
     class Program
     {
+        // Create a base class Car with two properties HP and Color
+        // Create a constructor setting those two properties
+        // Create a method called ShowDetails() which shows the HP and Color of the car on the console
+        // Create a Repair method which writes "Car was repaired" onto the console
+        // Create two deriving classes, BMW and Audi, which have their own constructor and have a additional property
+        // called Model. Also a private member called brand. Brand should be different in each of the two classes
+        // Create the to methods ShowDetails() and Repair in them as well Adjust those methods accordingly
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
