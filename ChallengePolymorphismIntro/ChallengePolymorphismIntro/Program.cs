893e640e4f79c121424483362966aa5045ffcbe0
Dominique Pendjol <d.pendjol@gmail.com>
2021-09-28 23:43:51 +0200

    Started Challenge Polymorphism Introduction

---
+++ ChallengePolymorphismIntro/ChallengePolymorphismIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChallengePolymorphismIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
