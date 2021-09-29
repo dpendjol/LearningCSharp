a6d376ddbd410eec25530315ce784b22826a54c8
Dominique Pendjol <d.pendjol@gmail.com>
2021-09-29 08:56:28 +0200

    Challenge Polymophism Introduction - added deriving classes BMW and Audi

---
+++ ChallengePolymorphismIntro/ChallengePolymorphismIntro/Audi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengePolymorphismIntro
+{
+    class Audi : Car
+    {
+        private string brand = "Audi";
+
+        public string Model { get; set; }
+
+        public Audi (int hp, string color, string model) : base (hp, color)
+        {
+            this.Model = model;
+        }
+    }
+}
---
+++ ChallengePolymorphismIntro/ChallengePolymorphismIntro/BMW.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengePolymorphismIntro
+{
+    class BMW : Car
+    {
+        // Members
+        private string Brand = "BMW";
+        public string Model { get; set; }
+
+        public BMW(int HP, string color, string model) : base(hp, color)
+        {
+            this.Model = model;
+        }
+    }
+}
